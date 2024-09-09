c97143415b856426ea349464ba126e513d66c795
John <55373552+PineappleHippy@users.noreply.github.com>
2024-09-08 19:00:11 -0500

    Create exampleProgram.java

---
+++ examples/exampleProgram.java
@@ -0,0 +1,10 @@
+// exampleProgram.java
+
+public class ExampleProgram {
+    public static void main(String[] args) {
+        System.out.println("Running example program...");
+        CommandCenter.printHelloWorld();
+        System.out.println("Reading example.txt...");
+        CommandCenter.printFileContent("example.txt");
+    }
+}
