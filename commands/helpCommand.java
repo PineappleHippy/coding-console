fb1b58f31c6913893e860046fdbf23583962bba8
John <55373552+PineappleHippy@users.noreply.github.com>
2024-09-08 19:40:29 -0500

    Create helpCommand.java
    Display help information

---
+++ commands/helpCommand.java
@@ -0,0 +1,10 @@
+// helpCommand.java
+
+public class HelpCommand {
+    public static void main(String[] args) {
+        System.out.println("HELP - Display help information");
+        System.out.println("DIR - Lists files and directories");
+        System.out.println("CD <dir> - Change directory");
+        System.out.println("CD.. - Go up one directory");
+    }
+}
